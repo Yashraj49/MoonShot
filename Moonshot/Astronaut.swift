2ebbcd55aa4a9d41a27b06367900268085c77e44
Yashraj Jadhav <yashrajjadhav49@gmail.com>
2023-02-03 20:41:03 +0530

    Initial Commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Yashraj jadhav on 01/02/23.
+//
+
+struct Astronaut : Codable , Identifiable {
+
+    let id : String
+    let name : String
+    let description : String
+}
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Yashraj jadhav on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
